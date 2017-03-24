84e30b23a69082937498214cf1c87bcc95d12fd0
Max Friedrich <max.friedrich@xing.com>
2017-03-24 10:59:27 +0100

    Open projects relative to workspace directory

--- lib/pandan/parser.rb
+++ lib/pandan/parser.rb
@@ -5,6 +5,7 @@ module Pandan
     attr_reader :workspace, :regex
 
     def initialize(workspace_path, filter)
+      @workspace_dir = File.dirname(@workspace_path)
       @workspace = Xcodeproj::Workspace.new_from_xcworkspace(workspace_path)
       @regex = filter
       @regex ||= ".*" # Match everything
@@ -12,7 +13,7 @@ module Pandan
 
     def all_targets()
       all_project_paths = workspace.file_references.map &:path
-      projects = all_project_paths.map { |project_path| Xcodeproj::Project.open(project_path) }
+      projects = all_project_paths.map { |project_path| Xcodeproj::Project.open(File.expand_path(project_path, @workspace_dir)) }
       all_targets = projects.flat_map { |project| project.targets }.select { |target| target.name =~ /#{regex}/ }
     end
   end
